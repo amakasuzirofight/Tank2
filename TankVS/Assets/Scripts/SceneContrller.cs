1cb8c6c8f92dd3e88fc72debdf4cb942095fc7b1
AMAKASU <ziropaku@gmail.com>
2023-01-29 22:34:49 +0900

    caemraの細かい設定

---
+++ TankVS/Assets/Scripts/SceneContrller.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneContrller : MonoBehaviour
+{
+
+    public static void ChangeGameScene()
+    {
+        SceneManager.LoadScene("");
+    }
+}
